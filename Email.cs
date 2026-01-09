b3e74788daeea6c888ecd4f48cf300f29c0ae71f
dheim123 <dheim@midland.k12.ia.us>
2026-01-09 17:47:54 +0000

    2

--- Email.cs
+++ Email.cs
@@ -1,6 +1,5 @@
 ﻿namespace knightmoves;
 public class Email
 {
-    public string introduction = "Dear Maurice. /n" +
-    "\tU \'highkey\' needa teach me how to do that \"dolphin laugh\" //";
+    public string introduction = "Dear Maurice.\n\tU 'highkey' needa teach me how to do that \"dolphin laugh\" \\";
 }
