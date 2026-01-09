e2474da259848adde1d687af561d4d935ad25f23
dheim123 <dheim@midland.k12.ia.us>
2026-01-09 17:49:54 +0000

    3

--- Email.cs
+++ Email.cs
@@ -1,5 +1,7 @@
 ﻿namespace knightmoves;
+
 public class Email
 {
+    // Class-level string with newline, tab, single quote, double quote, and backslash
     public string introduction = "Dear Maurice.\n\tU 'highkey' needa teach me how to do that \"dolphin laugh\" \\";
 }
